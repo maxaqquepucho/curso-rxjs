fe031c369db5fb229ab55f9fe3e584d2378e88e7
Max Aqquepucho <maxaqquepucho@gmail.com>
2021-01-12 22:01:59 -0500

    Add concatMap operator

---
+++ src/transformation/05-concatMap.ts
@@ -0,0 +1,18 @@
+import { fromEvent, interval } from "rxjs";
+import { concatMap, take } from "rxjs/operators";
+
+const interval$ = interval(500).pipe(take(3))
+const click$ = fromEvent(document, 'click')
+
+
+click$
+.pipe(
+    concatMap(() => interval$)
+)
+.subscribe(console.log)
+
+
+
+
+
+
