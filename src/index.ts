c3f9cad83206cd45d0b80d281d35628fc6f4380e
Max Aqquepucho <maxaqquepucho@gmail.com>
2021-01-21 21:49:22 -0500

    Add merge rxjs function

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,3 @@
-import { concat, interval, of } from "rxjs";
-import { take } from "rxjs/operators";
-
-
-const interval$ = interval(1000)
-
-concat(
-    interval$.pipe(take(3)),
-    interval$.pipe(take(2)),
-    of(7)
-)
-.subscribe(console.log)
-
-
 
 
 
